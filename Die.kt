fb9fb383306c4b2985460a2073fbfa4b6450d7d0
demimola24 <trideks0331>
2023-06-16 16:22:38 -0400

    add config

---
+++ Die.kt
@@ -0,0 +1,16 @@
+package game
+
+enum class ColorType {
+    RED,
+    WHITE,
+    YELLOW,
+    ORANGE,
+    BLACK
+}
+
+enum class DefaultSideType(val value: Int) {
+    SIX(6),
+    TEN(10),
+    TWENTY(20),
+    HUNDRED(100)
+}
